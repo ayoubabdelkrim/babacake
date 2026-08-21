82d3ff3315bee9da3bf144050d3864f3b81886fb
Ayoub Abdelkrim <abdelkrimayoub04@gmail.com>
2026-08-21 15:16:40 +0100

    Add Meta Pixel tracking

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Amiri, Cormorant_Garamond, Jost, Tajawal } from "next/font/google";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 
 import {
   DEFAULT_LOCALE,
@@ -81,9 +82,29 @@ export default async function RootLayout({
       dir={dirOf(lang)}
       className={`${cormorant.variable} ${jost.variable} ${amiri.variable} ${tajawal.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-ink text-cream">
-        {children}
-      </body>
-    </html>
-  );
+<body className="min-h-full flex flex-col bg-ink text-cream">
+  {children}
+
+  <Script
+    id="meta-pixel"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '3483653451797167');
+        fbq('track', 'PageView');
+      `,
+    }}
+  />
+</body>
+</html>
+);
 }
